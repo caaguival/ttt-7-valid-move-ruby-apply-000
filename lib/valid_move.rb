d11dffcf4cd78dde8e08ff8f1815c984d4d43d00
Carlos Aguilar Valderrama <carlos.aguilar.valderrama@gmail.com>
2020-05-13 07:36:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,6 +9,6 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
-  index= between?(0, 8)
+  index= be_between?(0, 8)
  board[index] != " " && board[index] != "" && board[index] != nil
 end
