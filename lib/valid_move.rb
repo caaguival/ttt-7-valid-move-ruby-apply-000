d480d3bc7ee0e3c72eea28c8b15ec3e8a8f11ca8
Carlos Aguilar Valderrama <carlos.aguilar.valderrama@gmail.com>
2020-05-13 07:34:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  index=.between[0, 8]
+  index= board.between?[0, 8]
  if position_taken?(board, index) == false
    "truth"
    else position_taken?(board, index) != [board, index.between?(0, 8)]
