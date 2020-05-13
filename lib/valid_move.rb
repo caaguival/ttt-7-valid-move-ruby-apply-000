905bd2db91a900480150d9b497b788c8f861ffaa
Carlos Aguilar Valderrama <carlos.aguilar.valderrama@gmail.com>
2020-05-13 07:00:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
  if position take?(board, index) == FALSE
-   then position_taken(board, index.between?(0, 8))
+  else position_taken(board, index.between?(0, 8))
+end
    
 end
 
