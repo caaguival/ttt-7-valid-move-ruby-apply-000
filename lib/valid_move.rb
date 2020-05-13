e3db965d9251a812ef8ec5ed62092de330a0bd20
Carlos Aguilar Valderrama <carlos.aguilar.valderrama@gmail.com>
2020-05-13 06:13:54 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,7 @@ def valid_move?(board, index)
   if position_taken?[board, index] == [0-8] 
   else
   end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
