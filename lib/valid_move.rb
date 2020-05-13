028f5b09b3308619e7fe5c37a1cfd6b3476267f3
Carlos Aguilar Valderrama <carlos.aguilar.valderrama@gmail.com>
2020-05-13 07:15:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,7 @@ def valid_move?(board, index)
  if position_taken?(board, index) == false
    "truth"
  else position_taken?(board, index) != index.between?(0, 8)
+   puts "Please choose again"
 end
    
 end
